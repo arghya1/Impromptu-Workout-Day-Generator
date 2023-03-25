5dd4896c5b75ee1658d698bf0e7ce4f9fa3d2be5
Arghya Basu <basuarghyadroid99@gmail.com>
2023-03-25 14:42:59 +0530

    Random Number v2

--- RandomNumberGenerator.c
+++ RandomNumberGenerator.c
@@ -1,8 +1,20 @@
+// This is a Random Number Generator Program
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
+int n;
 //Using rand() function to generate random number
-printf("Random Number you can use:%d", rand());
+printf("Random Number you can use between 1-31:");
+/* int i //declare in the first
+for (i=1; i<=31; i++)
+{
+	n = rand()%31 + 1;
+	printf("\t%d", n);
+}
+*/
+n=rand()%31 + 1;
+printf("\t%d", n);
+
 return 0;
 }
